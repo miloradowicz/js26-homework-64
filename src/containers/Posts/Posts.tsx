ea0867a0a6c1885f35e1cab159d5b95a4a6f4d8d
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-26 07:57:55 +0600

    Posts container added

---
+++ src/containers/Posts/Posts.tsx
@@ -0,0 +1,50 @@
+import { FC, useEffect, useState } from 'react';
+import { NamedPost } from '../../types';
+import { getPosts } from '../../lib/api';
+import Typography from '@mui/material/Typography';
+import PostCard from '../../components/PostCard/PostCard';
+import Stack from '@mui/material/Stack';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {}
+
+const Posts: FC<Props> = ({}) => {
+  const [posts, setPosts] = useState<NamedPost[]>([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      try {
+        const data = await getPosts();
+        setPosts(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    loadPosts();
+  }, []);
+
+  return (
+    <>
+      {!posts ? (
+        <Typography>There are no posts here yet.</Typography>
+      ) : (
+        <Stack spacing={1}>
+          {posts.map((x) => (
+            <PostCard
+              key={x.name}
+              post={x.post}
+              readMore={() => {
+                navigate(`/posts/${x.name}`);
+              }}
+            />
+          ))}
+        </Stack>
+      )}
+    </>
+  );
+};
+
+export default Posts;
