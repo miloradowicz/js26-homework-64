31d093d21ae978ad0c4d27ce4bdd932e75118715
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-26 07:57:37 +0600

    types updated

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,13 +3,18 @@ import { DateTime } from 'luxon';
 export interface Post {
   get title(): string;
   get body(): string;
-  get createdAt(): DateTime;
+  get createdOn(): DateTime;
+}
+
+export interface PostName {
+  get name(): string;
 }
 
 export interface PostContainer {
   [key: string]: Post;
 }
 
-export interface PostName {
+export interface NamedPost {
   get name(): string;
+  get post(): Post;
 }
