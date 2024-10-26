a66517a4b7bb1bdaf9ac3dcb4e3406df0bf953ef
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-26 07:57:28 +0600

    PostCard added

---
+++ src/components/PostCard/PostCard.tsx
@@ -0,0 +1,37 @@
+import { FC, memo } from 'react';
+import { Post } from '../../types';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+
+interface Props {
+  post: Post;
+  readMore: () => void;
+}
+
+const PostCard: FC<Props> = ({ post, readMore }) => {
+  console.debug('PostCard rendered');
+
+  return (
+    <Card variant='outlined' sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+          Created on:<Box>{post.createdOn}</Box>
+        </Typography>
+        <Typography variant='h5' component='div'>
+          {post.title}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small' onClick={readMore}>
+          Read More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default memo(PostCard);
